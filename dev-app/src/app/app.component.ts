e61d7b300fb63afa8cb0d0916a9e995cceb77a04
Chen Reuven <chen.rvn@gmail.com>
2020-07-02 16:28:12 +0300

    add fetch data do dev app for test API call

--- dev-app/src/app/app.component.ts
+++ dev-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dev-app';
+  todos: any[] = [];
+
+  ngOnInit(): void {
+    fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+      .then((res) => res.json())
+      .then((res) => {
+        this.todos = [...res];
+      });
+  }
 }
